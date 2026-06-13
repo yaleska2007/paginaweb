dbc1f0c139f5d9cfed8f4e89d3125658e4743f49
Gonzalo Javier Nino amaris <gonzalojaviernino@gmail.com>
2026-06-13 10:37:49 -0500

    Unificando firebase-config a la version 10.8.0

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDSDHg0VIb4EMfIwdToFPg6lFm4q2LUbU4",
@@ -17,7 +12,10 @@ const firebaseConfig = {
   appId: "1:860423923384:web:caa807ab5094370e1a764d"
 };
 
+// Inicializar la App de Firebase
 const app = initializeApp(firebaseConfig);
 
+// Exportar los servicios unificados en la versión 10.8.0
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const messaging = getMessaging(app);
